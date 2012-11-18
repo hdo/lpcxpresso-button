dbac583c71ac76695198d438f07198abb661c56a
Huy Do <huy.do@gmx.de>
2012-11-18 21:47:31 +0100

    added pin comment

--- lpcxpresso_cmsis2_button_interrupt/src/s0_input.h
+++ lpcxpresso_cmsis2_button_interrupt/src/s0_input.h
@@ -3,10 +3,10 @@
 
 // USING P0.23, P0.24, P0.25, P0.26; GPIO0!
 
-#define S0_INPUT0 (1 << 23)
-#define S0_INPUT1 (1 << 24)
-#define S0_INPUT2 (1 << 25)
-#define S0_INPUT3 (1 << 26)
+#define S0_INPUT0 (1 << 23) // PIN 15
+#define S0_INPUT1 (1 << 24) // PIN 16
+#define S0_INPUT2 (1 << 25) // PIN 17
+#define S0_INPUT3 (1 << 26) // PIN 18
 
 
 void s0_init(void);
